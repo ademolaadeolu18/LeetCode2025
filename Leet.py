01869f79e8591ba43f5a23bc62ed6e87b3dff9fa
Oluwatobi Ademola-Aliu <ademolaadeolu18@gmail.com>
2025-01-23 21:01:24 -0600

    Encode and Decode String

--- Leet.py
+++ Leet.py
@@ -127,3 +127,47 @@ def topkFrequent(words, k):
 words = ["the","day","is","sunny","the","the","the","sunny","is","is"]
 k = 4
 print(topkFrequent(words, k))
+
+
+
+
+
+# Encode and Decode Strings
+"""
+Solved 
+Design an algorithm to encode a list of strings to a single string. The encoded string is then decoded back to the original list of strings.
+
+Please implement encode and decode
+
+Example 1:
+
+Input: ["neet","code","love","you"]
+
+Output:["neet","code","love","you"]
+Example 2:
+
+Input: ["we","say",":","yes"]
+
+Output: ["we","say",":","yes"]
+"""
+
+def encode(strs):
+    res = ""
+    for s in strs:
+        res+= str(len(s)) + "#" + s
+    return res
+
+def decode(s):
+    res = []
+    i = 0
+    while i < len(s):
+        j = i
+        while s[j] != "#":
+            j+=1
+        length = int(s[i:j])
+        res.append(s[j+1: j+1 + length])
+        i = j+1+length
+    return res
+
+Input =  ["we","say",":","yes"]
+print(decode(encode(Input)))
