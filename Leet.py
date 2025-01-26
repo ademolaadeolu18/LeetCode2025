7ab4b2586b1728af866933806815dfb181de0662
Oluwatobi Ademola-Aliu <ademolaadeolu18@gmail.com>
2025-01-26 17:04:23 -0600

    Binary Search

--- Leet.py
+++ Leet.py
@@ -400,3 +400,46 @@ def longestConsecutive(nums):
 nums = [0,3,7,2,5,8,4,6,0,1]
 
 print(longestConsecutive(nums))
+
+
+
+# 9 Binary Search
+"""
+Solved 
+You are given an array of distinct integers nums, sorted in ascending order, and an integer target.
+
+Implement a function to search for target within nums. If it exists, then return its index, otherwise, return -1.
+
+Your solution must run in 
+O(logn)
+O(logn) time.
+
+Example 1:
+
+Input: nums = [-1,0,2,4,6,8], target = 4
+
+Output: 3
+Example 2:
+
+Input: 
+
+Output: -1
+"""
+
+def binarySearch(nums, target):
+    l = 0
+    r = len(nums) -1
+
+    while l <= r:
+        m = l + ((r - l)//2)
+        if nums[m] == target:
+            return m
+        elif nums[m] > target:
+            r = m - 1
+        else:
+            l = m+1
+    return -1
+
+nums = [-1,0,2,4,6,8]
+target = 3
+print(binarySearch(nums, target))
