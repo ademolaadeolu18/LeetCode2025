e53f0aa06b814544d8b6f88231e2860ed73c3fc6
Oluwatobi Ademola-Aliu <ademolaadeolu18@gmail.com>
2025-01-27 23:06:17 -0600

    3Sum

--- Leet.py
+++ Leet.py
@@ -591,4 +591,58 @@ def twosum(nums, target):
 
 nums = [-1,0]
 target = -1      
-print(twosum(nums, target))
+# print(twosum(nums, target))
+
+# 12  3Sum
+
+"""
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.
+
+ 
+
+Example 1:
+
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+Explanation: 
+nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+The distinct triplets are [-1,0,1] and [-1,-1,2].
+Notice that the order of the output and the order of the triplets does not matter.
+Example 2:
+
+Input: nums = [0,1,1]
+Output: []
+Explanation: The only possible triplet does not sum up to 0.
+Example 3:
+
+Input: nums = [0,0,0]
+Output: [[0,0,0]]
+Explanation: The only possible triplet sums up to 0.
+
+"""
+
+def threeSum(nums):
+    nums.sort()
+    res = set()
+
+    for i in range(len(nums)):
+        l = i+1
+        r = len(nums) - 1
+        while l < r:
+            currsum = nums[i] + nums[l] + nums[r]
+            if currsum == 0:
+                res.add((nums[i], nums[l],nums[r]))
+                l+=1
+                r-=1
+            elif currsum > 0:
+                r-=1
+            else:
+                l+=1
+    return list(res)
+
+nums = [0,0,0]
+print(threeSum(nums))
