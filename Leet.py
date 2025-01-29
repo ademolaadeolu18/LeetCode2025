ddd795ff33842139bf969d4fcdd817bfea72ee8b
Oluwatobi Ademola-Aliu <ademolaadeolu18@gmail.com>
2025-01-28 21:50:03 -0600

    Container with most water

--- Leet.py
+++ Leet.py
@@ -645,4 +645,46 @@ def threeSum(nums):
     return list(res)
 
 nums = [0,0,0]
-print(threeSum(nums))
+# print(threeSum(nums))
+
+
+
+# 13   Container With Most Water
+"""
+You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+
+Find two lines that together with the x-axis form a container, such that the container contains the most water.
+
+Return the maximum amount of water a container can store.
+
+Notice that you may not slant the container.
+
+ 
+
+Example 1:
+
+
+Input: height = [1,8,6,2,5,4,8,3,7]
+Output: 49
+Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+Example 2:
+
+Input: height = [1,1]
+Output: 1
+"""
+
+def maxArea(heights):
+    l = 0
+    r = len(heights) - 1
+    max_area = float("-inf")
+
+    while l < r:
+        area = min(heights[l], heights[r]) * (r - l)
+        max_area = max(max_area, area)
+        if heights[l] < heights[r]:
+            l+=1
+        else:
+            r-=1
+    return max_area
+heights = [1,1]
+print(maxArea(heights))
