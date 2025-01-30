0fd610d8ff6d84a9f433e6cac578366e7519794e
Oluwatobi Ademola-Aliu <ademolaadeolu18@gmail.com>
2025-01-30 00:12:54 -0600

    Evaluate Reverse Polish Notation

--- Leet.py
+++ Leet.py
@@ -730,4 +730,66 @@ def trap(height):
             area+= rightmax - height[r]
     return area
 height = [4,2,0,3,2,5]
-print(trap(height))
+# print(trap(height))
+
+
+# 15 Evaluate Reverse Polish Notation
+
+"""
+You are given an array of strings tokens that represents an arithmetic expression in a Reverse Polish Notation.
+
+Evaluate the expression. Return an integer that represents the value of the expression.
+
+Note that:
+
+The valid operators are '+', '-', '*', and '/'.
+Each operand may be an integer or another expression.
+The division between two integers always truncates toward zero.
+There will not be any division by zero.
+The input represents a valid arithmetic expression in a reverse polish notation.
+The answer and all the intermediate calculations can be represented in a 32-bit integer.
+ 
+
+Example 1:
+
+Input: tokens = ["2","1","+","3","*"]
+Output: 9
+Explanation: ((2 + 1) * 3) = 9
+Example 2:
+
+Input: tokens = ["4","13","5","/","+"]
+Output: 6
+Explanation: (4 + (13 / 5)) = 6
+Example 3:
+
+Input: tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+Output: 22
+Explanation: ((10 * (6 / ((9 + 3) * -11))) + 17) + 5
+= ((10 * (6 / (12 * -11))) + 17) + 5
+= ((10 * (6 / -132)) + 17) + 5
+= ((10 * 0) + 17) + 5
+= (0 + 17) + 5
+= 17 + 5
+= 22
+"""
+
+def evalRPN(tokens):
+    s = []
+    for c in  tokens:
+        if c == "+":
+            s.append(s.pop() + s.pop())
+        elif c == "*":
+            s.append(s.pop() * s.pop())
+        elif c == "-":
+            b, a = s.pop(), s.pop()
+            s.append(a - b)
+        elif c == "/":
+            b, a = s.pop(), s.pop()
+            s.append(int(a / b))
+        else:
+            s.append(int(c))
+    return s[0]
+
+tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+print(evalRPN(tokens))
+
