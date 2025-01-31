1c1082de5d5690ea41b4c2f8096009a302458568
Oluwatobi Ademola-Aliu <ademolaadeolu18@gmail.com>
2025-01-30 21:01:06 -0600

    Daily Temperatures

--- Leet.py
+++ Leet.py
@@ -835,5 +835,39 @@ def generateParenthesis(n):
     return res
 
 n = 5
-print(generateParenthesis(n))
+# print(generateParenthesis(n))
+
+#17  Daily Temperatures
+
+"""
+Given an array of integers temperatures represents the daily temperatures, return an array answer such that answer[i] is the number of days you have to wait after the ith day to get a warmer temperature. If there is no future day for which this is possible, keep answer[i] == 0 instead.
+
+ 
+
+Example 1:
+
+Input: temperatures = [73,74,75,71,69,72,76,73]
+Output: [1,1,4,2,1,1,0,0]
+Example 2:
+
+Input: temperatures = [30,40,50,60]
+Output: [1,1,1,0]
+Example 3:
+
+Input: temperatures = [30,60,90]
+Output: [1,1,0]
+"""
+
+def dailyTemps(temperatures):
+    res = [0] * len(temperatures)
+    stack = []
+    for i , t in enumerate(temperatures):
+        while stack and  t > stack[-1][0]:
+            stk_tmp , stk_idx = stack.pop()
+            res[stk_idx] = i - stk_idx
+        stack.append([t, i])
+    return res
+
+temperatures = [30,40,50,60]
+print(dailyTemps(temperatures))
 
