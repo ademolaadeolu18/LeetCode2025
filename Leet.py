ff79eece427a443a5edfdad7c22518878344073b
Oluwatobi Ademola-Aliu <ademolaadeolu18@gmail.com>
2025-01-31 22:54:21 -0600

    Largest Rectangle In Histogram

--- Leet.py
+++ Leet.py
@@ -933,6 +933,47 @@ def carFleet(target, position, speed):
 
 target , position , speed =  12, [10,8,0,5,3], [2,4,1,1,3]
 
-print(carFleet(target, position, speed))
+# print(carFleet(target, position, speed))
 
 
+#  19  Largest Rectangle in Histogram
+
+"""
+Given an array of integers heights representing the histogram's bar height where the width of each bar is 1, return the area of the largest rectangle in the histogram.
+
+ 
+
+Example 1:
+
+
+Input: heights = [2,1,5,6,2,3]
+Output: 10
+Explanation: The above is a histogram where width of each bar is 1.
+The largest rectangle is shown in the red area, which has an area = 10 units.
+Example 2:
+
+
+Input: heights = [2,4]
+Output: 4
+
+"""
+
+def largestRectangleArea(heights):
+    maxArea = 0
+    stack = []
+
+    for i ,h in enumerate(heights):
+        start = i
+        while stack and stack[-1][1] > h:
+            idx, height = stack.pop()
+            maxArea = max(maxArea, height * (i - idx))
+            start = idx
+        stack.append((start, h))
+    
+    for i , h in stack:
+        maxArea = max(maxArea, h *(len(heights)- i))
+    
+    return maxArea
+    
+heights = [2,1,5,6,2,3]
+print(largestRectangleArea(heights))
