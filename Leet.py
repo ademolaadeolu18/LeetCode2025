df6dc2d9311f9e89aa2f57a08fe253556a9d1fe2
Oluwatobi Ademola-Aliu <ademolaadeolu18@gmail.com>
2025-02-03 22:26:18 -0600

    Best Time To Buy and Sell stock

--- Leet.py
+++ Leet.py
@@ -1026,7 +1026,7 @@ def minEatingRate(piles, h):
     return res
 
 piles , h = [30,11,23,4,20] , 6
-print(minEatingRate(piles, h))
+# print(minEatingRate(piles, h))
 
 
 #   21   Find Minimum in Rotated Sorted Array
@@ -1182,9 +1182,48 @@ class TimeMap:
         return res
 
 timeMap = TimeMap()
-print(timeMap.set("foo", "bar", 1))
-print(timeMap.get("foo", 1))
-print(timeMap.get("foo", 3))
-timeMap.set("foo", "bar2", 4)
-print(timeMap.get("foo", 4))
-print(timeMap.get("foo", 5))
+# print(timeMap.set("foo", "bar", 1))
+# print(timeMap.get("foo", 1))
+# print(timeMap.get("foo", 3))
+# timeMap.set("foo", "bar2", 4)
+# print(timeMap.get("foo", 4))
+# print(timeMap.get("foo", 5))
+
+
+# 24  Best Time to Buy and Sell Stock
+"""
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+ 
+
+Example 1:
+
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+Example 2:
+
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transactions are done and the max profit = 0.
+
+"""
+def maxProfit(prices):
+    maxprof = 0
+    l , r = 0 , 1
+    while r < len(prices):
+        if prices[l] < prices[r]:
+            maxprof = max(maxprof, prices[r] - prices[l])
+        else:
+            l = r
+        r+=1
+    return maxprof
+
+
+prices = [7,6,4,3,1]
+print(maxProfit(prices))
