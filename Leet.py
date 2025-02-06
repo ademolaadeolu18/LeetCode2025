4c1c1a8101bcb15f2dfbb2081442a772cd84fb07
Oluwatobi Ademola-Aliu <ademolaadeolu18@gmail.com>
2025-02-05 19:18:36 -0600

    Meeting Rooms

--- Leet.py
+++ Leet.py
@@ -1309,3 +1309,47 @@ def characterReplacement(s, k):
 s = "AABABBA"
 k = 1
 print(characterReplacement(s, k))
+
+
+#27 Meeting Rooms
+
+"""
+Given an array of meeting time interval objects consisting of start and end times [[start_1,end_1],[start_2,end_2],...] (start_i < end_i), determine if a person could add all meetings to their schedule without any conflicts.
+
+Example 1:
+
+Input: intervals = [(0,30),(5,10),(15,20)]
+
+Output: false
+Explanation:
+
+(0,30) and (5,10) will conflict
+(0,30) and (15,20) will conflict
+Example 2:
+
+Input: intervals = [(5,8),(9,15)]
+
+Output: true
+"""
+
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+def canAttendMeetings(intervals) -> bool:
+    intervals.sort(key=lambda i:i[0])
+
+    for i in range(1, len(intervals)):
+        l1 = intervals[i-1]
+        l2 = intervals[i]
+
+        if l1[1] > l2[0]:
+            return False
+    return True
+
+
+intervals = [(0,30),(5,10),(15,20)]
+print(canAttendMeetings(intervals))
