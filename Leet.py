862090ff2e051f296d9f4a81b0896883c2e44cb6
Oluwatobi Ademola-Aliu <ademolaadeolu18@gmail.com>
2025-02-07 19:20:58 -0600

    Non-Ovwerlapping Intervals

--- Leet.py
+++ Leet.py
@@ -1308,7 +1308,7 @@ def characterReplacement(s, k):
 
 s = "AABABBA"
 k = 1
-print(characterReplacement(s, k))
+# print(characterReplacement(s, k))
 
 
 #27 Meeting Rooms
@@ -1438,4 +1438,43 @@ def merge(intervals):
     return res
 
 intervals = [[1,2],[2,3], [3,7], [6, 10]]
-print(merge(intervals))
+# print(merge(intervals))
+
+
+
+#  29   Non-overlapping Intervals
+
+"""
+Given an array of intervals intervals where intervals[i] = [start_i, end_i], return the minimum number of intervals you need to remove to make the rest of the intervals non-overlapping.
+
+Note: Intervals are non-overlapping even if they have a common point. For example, [1, 3] and [2, 4] are overlapping, but [1, 2] and [2, 3] are non-overlapping.
+
+Example 1:
+
+Input: intervals = [[1,2],[2,4],[1,4]]
+
+Output: 1
+Explanation: After [1,4] is removed, the rest of the intervals are non-overlapping.
+
+Example 2:
+
+Input: intervals = [[1,2],[2,4]]
+
+Output: 0
+"""
+
+def eraseOverlappingIntervals(intervals):
+    intervals.sort()
+    res = 0
+    prevEnd = intervals[0][1]
+
+    for start, end in intervals[1:]:
+        if start >= prevEnd:
+            prevEnd = end
+        else:
+            res+=1
+            prevEnd = min(prevEnd, end)
+    return res
+
+intervals = [[1,2],[2,4]]
+print(eraseOverlappingIntervals(intervals))
