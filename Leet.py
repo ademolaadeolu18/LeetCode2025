7374f9fcc9d38e7ad0658d52f7b06bd64a765c5c
Oluwatobi Ademola-Aliu <ademolaadeolu18@gmail.com>
2025-02-08 19:53:10 -0600

    Linked List Cycle Detection

--- Leet.py
+++ Leet.py
@@ -1518,3 +1518,44 @@ def minMeetingRooms(intervals):
 intervals = [[0,40],[5,10],[15,20]]
 
 print(minMeetingRooms(intervals))
+
+
+#   31  Has Cycle
+
+"""
+Given the beginning of a linked list head, return true if there is a cycle in the linked list. Otherwise, return false.
+
+There is a cycle in a linked list if at least one node in the list that can be visited again by following the next pointer.
+
+Internally, index determines the index of the beginning of the cycle, if it exists. The tail node of the list will set it's next pointer to the index-th node. If index = -1, then the tail node points to null and no cycle exists.
+
+Note: index is not given to you as a parameter.
+
+Example 1:
+
+
+
+Input: head = [1,2,3,4], index = 1
+
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+
+Example 2:
+
+
+
+Input: head = [1,2], index = -1
+
+Output: false
+"""
+
+def hasCycle(head) -> bool:
+    slow = head
+    fast = head
+
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+        if slow == fast:
+            return True
+    return False
