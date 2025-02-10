232c007a0f732d9046c124fdf76e4e3f5512240d
Oluwatobi Ademola-Aliu <ademolaadeolu18@gmail.com>
2025-02-09 23:51:08 -0600

    Pascals Triangle 2

--- Leet.py
+++ Leet.py
@@ -1609,4 +1609,42 @@ def clearDigits(s):
 
 
 s = "adbf34ghcb34"
-print(clearDigits(s))
+# print(clearDigits(s))
+
+
+#33  Pascals Triangele
+
+"""
+Given an integer rowIndex, return the rowIndexth (0-indexed) row of the Pascal's triangle.
+
+In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+
+
+ 
+
+Example 1:
+
+Input: rowIndex = 3
+Output: [1,3,3,1]
+Example 2:
+
+Input: rowIndex = 0
+Output: [1]
+Example 3:
+
+Input: rowIndex = 1
+Output: [1,1]
+"""
+
+def pascalsTriange2(rowIndex):
+    res = [1]
+    for i in range(rowIndex):
+        nextRow = [0] * (len(res) + 1)
+        for j in range(len(res)):
+            nextRow[j] +=res[j]
+            nextRow[j+1] += res[j]
+        res = nextRow
+    return res
+
+rowIndex = 3
+print(pascalsTriange2(rowIndex))
