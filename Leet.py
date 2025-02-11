9e24cbe5ca641f2b592b80cb3fcddd3c8b42f781
Oluwatobi Ademola-Aliu <ademolaadeolu18@gmail.com>
2025-02-10 21:43:17 -0600

    Reorder List

--- Leet.py
+++ Leet.py
@@ -1648,3 +1648,68 @@ def pascalsTriange2(rowIndex):
 
 rowIndex = 3
 print(pascalsTriange2(rowIndex))
+
+
+#  34 Reorder Linked List
+
+"""
+You are given the head of a singly linked-list.
+
+The positions of a linked list of length = 7 for example, can intially be represented as:
+
+[0, 1, 2, 3, 4, 5, 6]
+
+Reorder the nodes of the linked list to be in the following order:
+
+[0, 6, 1, 5, 2, 4, 3]
+
+Notice that in the general case for a list of length = n the nodes are reordered to be in the following order:
+
+[0, n-1, 1, n-2, 2, n-3, ...]
+
+You may not modify the values in the list's nodes, but instead you must reorder the nodes themselves.
+
+Example 1:
+
+Input: head = [2,4,6,8]
+
+Output: [2,8,4,6]
+Example 2:
+
+Input: head = [2,4,6,8,10]
+
+Output: [2,10,4,8,6]
+"""
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+    def reorderList(self, head):
+        slow = head
+        fast = head.next
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        second = slow.next
+        prev = None
+        slow.next = None
+
+        while second:
+            temp = second.next
+            second.next = prev
+            prev = second
+            second = temp
+
+        first = head
+        second = prev
+
+        while second:
+            temp1 = first.next
+            temp2 = second.next
+            first.next = second
+            second.next = temp1
+            first = temp1
+            second = temp2
