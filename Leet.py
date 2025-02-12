5d31b84877940bd1a93c74c41055241fe0c3c6bf
Oluwatobi Ademola-Aliu <ademolaadeolu18@gmail.com>
2025-02-11 21:29:00 -0600

    Remove Node From End Of LinkedList

--- Leet.py
+++ Leet.py
@@ -1713,3 +1713,47 @@ class ListNode:
             second.next = temp1
             first = temp1
             second = temp2
+
+
+
+#  35 Remove Node From End Of Linked List
+
+
+"""
+You are given the beginning of a linked list head, and an integer n.
+
+Remove the nth node from the end of the list and return the beginning of the list.
+
+Example 1:
+
+Input: head = [1,2,3,4], n = 2
+
+Output: [1,2,4]
+Example 2:
+
+Input: head = [5], n = 1
+
+Output: []
+Example 3:
+
+Input: head = [1,2], n = 2
+
+Output: [2]
+"""
+
+def removeNthNodeFromEnd(head, n):
+    dummy = ListNode(0, head)
+    left = dummy
+    right = head
+
+    while n > 0 and right:
+        right = right.next
+        n-=1
+
+    while right:
+        right = right.next
+        left = left.next
+    
+    left.next = left.next.next
+
+    return dummy.next
