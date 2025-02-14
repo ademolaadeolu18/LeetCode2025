fd0c3b2079e5d58ab2766c2d425ab96c4102ed46
Oluwatobi Ademola-Aliu <ademolaadeolu18@gmail.com>
2025-02-13 18:58:39 -0600

    Invert Binary Tree

--- Leet.py
+++ Leet.py
@@ -1871,3 +1871,25 @@ def findDuplicate2(nums):
 
 nums = [1,2,3,4,4]
 print(findDuplicate2(nums))
+
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def invertTree(root):
+    if not root:
+        return None
+    
+    tmp = root.left
+    root.left = root.right
+    root.right = tmp
+
+    invertTree(root.left)
+    invertTree(root.right)
+    return root
+
+
