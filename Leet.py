36375c9f5a8fcb1e2be8ef6aecb5788bc5f49af0
Oluwatobi Ademola-Aliu <ademolaadeolu18@gmail.com>
2025-02-15 17:47:20 -0600

    Balanced Binary Tree

--- Leet.py
+++ Leet.py
@@ -1958,3 +1958,42 @@ def diameterOfBinaryTree(root):
         return 1 + max(left, right)
     dfs(root)
     return res
+
+
+
+#  41 Balanced Binary Tree
+
+"""Given a binary tree, return true if it is height-balanced and false otherwise.
+
+A height-balanced binary tree is defined as a binary tree in which the left and right subtrees of every node differ in height by no more than 1.
+
+Example 1:
+
+
+
+Input: root = [1,2,3,null,null,4]
+
+Output: true
+Example 2:
+
+
+
+Input: root = [1,2,3,null,null,4,null,5]
+
+Output: false
+Example 3:
+
+Input: root = []
+
+Output: true
+"""
+def isBalanced(root):
+    def dfs(curr):
+        if not curr:
+            return [True, 0]
+        left = dfs(curr.left)
+        right = dfs(curr.right)
+
+        balanced = left[0] and right[0] and abs(left[1] - right[1]) <=1
+        return [balanced, 1 + max(left[1], right[1])]
+    return dfs(root)[0]
