c85fd75336ce92b1e4c135bfc7487c0d236ed06b
Oluwatobi Ademola-Aliu <ademolaadeolu18@gmail.com>
2025-02-16 20:11:18 -0600

    Is Sub Tree

--- Leet.py
+++ Leet.py
@@ -2036,3 +2036,40 @@ def isSameTree(p, q):
     if not p or not q or p.val != q.val:
         return False
     return ((isSameTree(p.left, q.left)) and (p.right, q.right))
+
+
+# 42 SubTree Of Another Tree
+
+"""
+Given the roots of two binary trees root and subRoot, return true if there is a subtree of root with the same structure and node values of subRoot and false otherwise.
+
+A subtree of a binary tree tree is a tree that consists of a node in tree and all of this node's descendants. The tree tree could also be considered as a subtree of itself.
+
+ 
+
+Example 1:
+
+
+Input: root = [3,4,5,1,2], subRoot = [4,1,2]
+Output: true
+Example 2:
+
+
+Input: root = [3,4,5,1,2,null,null,null,null,0], subRoot = [4,1,2]
+Output: false
+
+"""
+def isSubTree(root, subRoot):
+    if not subRoot:
+        return True
+    if not s:
+        return False
+    if sameTree(root, subRoot):
+        return True
+    return isSubTree(root.left, subRoot) or isSubTree(root.right, subRoot)
+def sameTree(s, t):
+    if not s and not t:
+        return True
+    if not s or not t or s.val != t.val:
+        return False
+    return ((sameTree(s.left, t.left)) and (sameTree(s.right, t.right)))
