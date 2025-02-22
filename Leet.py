1517699ac2929980b502ff7f57ecbade58e31526
Oluwatobi Ademola-Aliu <ademolaadeolu18@gmail.com>
2025-02-21 20:52:56 -0600

    Build Tree

--- Leet.py
+++ Leet.py
@@ -2376,3 +2376,33 @@ def KthSmallest(root, k):
     return res
 
 
+#    49  Construct Binary Tree from Preorder and Inorder Traversal
+
+"""
+
+Solved
+Medium
+Topics
+Companies
+Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary tree and inorder is the inorder traversal of the same tree, construct and return the binary tree.
+
+
+Example 1:
+
+Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+Output: [3,9,20,null,null,15,7]
+Example 2:
+
+Input: preorder = [-1], inorder = [-1]
+Output: [-1]
+"""
+def buildTree(preorder, inorder):
+    if not preorder or not inorder:
+        return None
+    
+    root = TreeNode(preorder[0])
+    mid = inorder.index(preorder[0])
+    root.left = buildTree(preorder[1:mid+1], inorder[:mid])
+    root.right = buildTree(preorder[mid+1:], inorder[mid+1:])
+
+    return root
