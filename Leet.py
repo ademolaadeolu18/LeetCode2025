2bd27fdf5b02be36ce8bef1f33b47b4619d67a8c
Oluwatobi Ademola-Aliu <ademolaadeolu18@gmail.com>
2025-02-22 13:49:33 -0600

    Permutation in String

--- Leet.py
+++ Leet.py
@@ -2406,3 +2406,65 @@ def buildTree(preorder, inorder):
     root.right = buildTree(preorder[mid+1:], inorder[mid+1:])
 
     return root
+
+
+
+#   50    Permutation in String
+
+"""
+
+Solved
+Medium
+Topics
+Companies
+Hint
+Given two strings s1 and s2, return true if s2 contains a 
+permutation
+ of s1, or false otherwise.
+
+In other words, return true if one of s1's permutations is the substring of s2.
+
+ 
+
+Example 1:
+
+Input: s1 = "ab", s2 = "eidbaooo"
+Output: true
+Explanation: s2 contains one permutation of s1 ("ba").
+Example 2:
+
+Input: s1 = "ab", s2 = "eidboaoo"
+Output: false
+"""
+def checkInclusion(s1, s2):
+    if len(s1) > len(s2): return False
+    s1Count , s2Count = [0] * 26 , [0] * 26
+    for i in range(len(s1)):
+        s1Count[ord(s1[i]) - ord('a')] +=1
+        s2Count[ord(s2[i]) - ord('a')] +=1
+    matches = 0
+    for i in range(26):
+        matches += (1 if s1Count[i] == s2Count[i] else 0)
+    l = 0
+    for r in range(len(s1), len(s2)):
+        if matches == 26: return  True
+        index = ord(s2[r]) - ord('a')
+        s2Count[index] +=1
+        if s1Count[index] == s2Count[index]:
+            matches +=1
+        elif s1Count[index] + 1 == s2Count[index]:
+            matches -=1
+        index = ord(s2[l]) - ord('a')
+        s2Count[index] -=1
+        if s1Count[index] == s2Count[index]:
+            matches +=1
+        elif s1Count[index] - 1 == s2Count[index]:
+            matches -=1
+        l+=1
+    return matches == 26
+
+
+s1 = "ab" 
+s2 = "eidbobaoo"
+
+print(checkInclusion(s1, s2))
