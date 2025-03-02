1fe37cb2e9ec1593baa3f7b7d33326cf5179ac0e
Oluwatobi Ademola-Aliu <ademolaadeolu18@gmail.com>
2025-03-01 19:48:27 -0600

    Combination Sum II

--- Leet.py
+++ Leet.py
@@ -2584,7 +2584,7 @@ def isPalindrome2(x):
         num//=10
     return x == reverse
 
-print(isPalindrome2(x))
+# print(isPalindrome2(x))
 
 
 
@@ -2647,7 +2647,7 @@ def romanToInt(s):
     return res
 
 s = "MCMXCIV"
-print(romanToInt(s))
+# print(romanToInt(s))
 
 
 # 54   Median of Two Sorted Arrays
@@ -2704,7 +2704,7 @@ def medianSortedArray(nums1, nums2):
 nums1 = [1,2]
 nums2 = [3,4]
 
-print(medianSortedArray(nums1, nums2))\
+# print(medianSortedArray(nums1, nums2))\
 
 
 
@@ -2760,7 +2760,7 @@ def combination(candidates, target):
 
 candidates = [2,3,5]
 target = 8
-print(combination(candidates, target))
+# print(combination(candidates, target))
 
 
 #  56   Subsets
@@ -2798,4 +2798,59 @@ def subsets(nums):
     return res
 
 nums = [1,2,3]
-print(subsets(nums))
+# print(subsets(nums))
+
+
+#  57   Combination Sum 2
+
+"""
+Given a collection of candidate numbers (candidates) and a target number (target), find all unique combinations in candidates where the candidate numbers sum to target.
+
+Each number in candidates may only be used once in the combination.
+
+Note: The solution set must not contain duplicate combinations.
+
+ 
+
+Example 1:
+
+Input: candidates = [10,1,2,7,6,1,5], target = 8
+Output: 
+[
+[1,1,6],
+[1,2,5],
+[1,7],
+[2,6]
+]
+Example 2:
+
+Input: candidates = [2,5,2,1,2], target = 5
+Output: 
+[
+[1,2,2],
+[5]
+]
+"""
+def comninationSum2(candidates, target):
+    candidates.sort()
+    res = []
+    curr = []
+    def dfs(i):
+        if sum(curr) == target:
+            res.append(curr.copy())
+            return
+        if sum(curr) > target or i >= len(candidates):
+            return
+        curr.append(candidates[i])
+        dfs(i+1)
+        curr.pop()
+        while i + 1 < len(candidates) and candidates[i] == candidates[i+1]:
+            i+=1
+        dfs(i+1)
+    dfs(0)
+    return res
+
+
+candidates = [2,5,2,1,2]
+target = 5
+print(comninationSum2(candidates, target))
