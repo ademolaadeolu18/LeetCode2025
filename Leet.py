0092eac8d8ca7c3b545758941a4ba27b44a6b2d8
Oluwatobi Ademola-Aliu <ademolaadeolu18@gmail.com>
2025-03-07 20:32:28 -0600

    Solve N-Queens

--- Leet.py
+++ Leet.py
@@ -3121,4 +3121,66 @@ def letterComb(digits):
 
 
 digits = "23"
-print(letterComb(digits))
+# print(letterComb(digits))
+
+
+#  63 N-Queens
+
+"""
+
+Solved
+Hard
+Topics
+Companies
+The n-queens puzzle is the problem of placing n queens on an n x n chessboard such that no two queens attack each other.
+
+Given an integer n, return all distinct solutions to the n-queens puzzle. You may return the answer in any order.
+
+Each solution contains a distinct board configuration of the n-queens' placement, where 'Q' and '.' both indicate a queen and an empty space, respectively.
+
+ 
+
+Example 1:
+
+
+Input: n = 4
+Output: [[".Q..","...Q","Q...","..Q."],["..Q.","Q...","...Q",".Q.."]]
+Explanation: There exist two distinct solutions to the 4-queens puzzle as shown above
+Example 2:
+
+Input: n = 1
+Output: [["Q"]]
+"""
+def solveNQueens(n):
+    col = set()
+    posDiag = set()
+    negDiag = set()
+
+    board = [["."] * n for i in range(n)]
+    res = []
+
+    def backtrack(r):
+        if r == n:
+            res.append(["".join(row) for row in board])
+            return
+        for c in range(n):
+            if c in col or (r+c) in posDiag or (r-c) in negDiag:
+                continue
+
+            col.add(c)
+            posDiag.add(r+c)
+            negDiag.add(r-c)
+            board[r][c] = "Q"
+
+            backtrack(r + 1)
+
+            col.remove(c)
+            posDiag.remove(r+c)
+            negDiag.remove(r-c)
+            board[r][c] = "."
+    backtrack(0)
+    return res
+
+n = 4
+print(solveNQueens(n))
+
