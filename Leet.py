94dc50c9118bc52fdfea268e2caeff0f6efbfa87
Oluwatobi Ademola-Aliu <ademolaadeolu18@gmail.com>
2025-03-17 23:10:35 -0500

    Coin Change

--- Leet.py
+++ Leet.py
@@ -3460,8 +3460,52 @@ def countSubstrings(s):
         res+= palindrome(i , i+1)
     return res
 
-s = "aaa"
-print(countSubstrings(s))
+# s = "aaa"
+# print(countSubstrings(s))
 
 
 ## Revised a couple of past solutions
+
+#70   Coin Change
+
+"""
+
+Solved 
+You are given an integer array coins representing coins of different denominations (e.g. 1 dollar, 5 dollars, etc) and an integer amount representing a target amount of money.
+
+Return the fewest number of coins that you need to make up the exact target amount. If it is impossible to make up the amount, return -1.
+
+You may assume that you have an unlimited number of each coin.
+
+Example 1:
+
+Input: coins = [1,5,10], amount = 12
+
+Output: 3
+Explanation: 12 = 10 + 1 + 1. Note that we do not have to use every kind coin available.
+
+Example 2:
+
+Input: coins = [2], amount = 3
+
+Output: -1
+Explanation: The amount of 3 cannot be made up with coins of 2.
+
+Example 3:
+
+Input: coins = [1], amount = 0
+
+Output: 0
+"""
+def coinChange(coins, amount):
+    dp = [amount +1] * (amount + 1)
+    dp[0] = 0
+
+    for a in range(1, amount+1):
+        for c in coins:
+            dp[a] = min(dp[a], 1 + dp[a-c])
+    return dp[-1] if dp[-1] != amount+1 else -1
+coins = [1,5,10]
+amount = 12
+print(coinChange(coins, amount))
+
