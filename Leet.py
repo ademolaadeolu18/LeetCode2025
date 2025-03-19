8ce6e3cc8fe304c1d6f1bc26124ed6bed02e88d0
Oluwatobi Ademola-Aliu <ademolaadeolu18@gmail.com>
2025-03-19 13:03:42 -0500

    Maximum Subarray

--- Leet.py
+++ Leet.py
@@ -3558,5 +3558,42 @@ def decode(s):
     return dfs(0)
 
 s = "12"
-print(decode(s))
-    
+# print(decode(s))
+
+
+#  72   Maximum Product Subarray
+"""
+
+Solved
+Medium
+Topics
+Companies
+Given an integer array nums, find a subarray that has the largest product, and return the product.
+
+The test cases are generated so that the answer will fit in a 32-bit integer.
+
+Example 1:
+
+Input: nums = [2,3,-2,4]
+Output: 6
+Explanation: [2,3] has the largest product 6.
+Example 2:
+
+Input: nums = [-2,0,-1]
+Output: 0
+Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
+"""
+def maxProd(nums):
+    currentmin = 1
+    currentmax = 1
+    res = float("-inf")
+
+    for n in nums:
+        tmp = n * currentmax
+        currentmax = max(n*currentmax, n*currentmin, n)
+        currentmin = min(n*currentmin, tmp, n)
+        res = max(res, currentmax)
+    return res
+
+nums = [2,3,-2,4]
+print(maxProd(nums))
